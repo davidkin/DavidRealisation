c1ade6b83979ffaae46b183117780b7c0100e412
David <davidfloyd322@gmail.com>
2018-12-22 14:59:13 +0200

    bug/eslint_fixes, develop branch was backuped

--- src/tests/forEach.test.js
+++ src/tests/forEach.test.js
@@ -75,10 +75,10 @@ describe('tests for method forEach', () => {
 
   test('thisArg is set as this of callbackFunction properly for forEach method ', () => {
     const testArr = [];
+
     const user2 = {
       name: 'ivan2'
     };
-
     const user = {
       name: 'ivan',
       testForEach() {
@@ -89,6 +89,7 @@ describe('tests for method forEach', () => {
     };
 
     user.testForEach();
+
     expect(testArr).toEqual(['ivan2', 'ivan2', 'ivan2']);
   });
 
