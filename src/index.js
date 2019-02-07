d5d7b00f665b02cc2ce8f046bfef422adcc9ad3f
David <davidfloyd322@gmail.com>
2019-02-07 17:15:33 +0200

    feature/developer-ts: add index.js for tests checks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ class MyArray {
   constructor(...values) {
     if (values.length === 1 && typeof values[0] === 'number') {
       this.length = values[0];
-    } else {
+    }
+    else {
       this.length = values.length;
 
       for (let i = 0; i < values.length; i++) {
@@ -10,9 +11,7 @@ class MyArray {
       }
     }
   }
-
   // -------------------------------------
-
   static from(arg, callback, thisArg) {
     const resultMassive = new MyArray();
 
@@ -21,7 +20,6 @@ class MyArray {
         resultMassive[i] = callback.call(thisArg, arg[i], i, arg);
         resultMassive.length += 1;
       }
-
       return resultMassive;
     }
 
@@ -29,20 +27,15 @@ class MyArray {
       resultMassive[i] = arg[i];
       resultMassive.length += 1;
     }
-
     return resultMassive;
   }
-
-
   push(...values) {
     for (let i = 0; i < values.length; i++) {
       this[this.length] = values[i];
       this.length += 1;
     }
-
     return this.length;
   }
-
   pop() {
     if (this.length === 0) {
       return;
@@ -50,18 +43,14 @@ class MyArray {
 
     const elem = this[this.length - 1];
     delete this[this.length - 1];
-
     this.length -= 1;
-
     return elem;
   }
-
   forEach(callback, thisArg) {
     for (let i = 0; i < this.length; i++) {
       callback.call(thisArg, this[i], i, this);
     }
   }
-
   map(callback, thisArg) {
     const newArr = new MyArray();
     newArr.length = this.length;
@@ -69,10 +58,8 @@ class MyArray {
     for (let i = 0; i < this.length; i++) {
       newArr[i] = callback.call(thisArg, this[i], i, this);
     }
-
     return newArr;
   }
-
   filter(callback, thisArg) {
     const newArr = new MyArray();
 
@@ -82,22 +69,17 @@ class MyArray {
         newArr.length += 1;
       }
     }
-
     return newArr;
   }
-
   toString() {
     let newStr = '';
 
     for (let i = 0; i < this.length - 1; i++) {
       newStr += `${this[i]},`;
     }
-
     newStr += this[this.length - 1];
-
     return this.length === 0 ? '' : newStr;
   }
-
   reduce(callback, startValue) {
     let acc = startValue === undefined ? this[0] : startValue;
 
@@ -117,35 +99,37 @@ class MyArray {
       acc = callback(startValue, this[0], 0, this);
     }
 
-
     for (let i = 1; i < this.length; i++) {
       acc = callback(acc, this[i], i, this);
     }
-
     return acc;
   }
-
   sort(callback) {
     let cb = callback;
 
     if (!cb) {
-      cb = (a, b) => {
+      cb = function(a, b) {
         const a1 = String(a);
         const b1 = String(b);
 
         if (a1 > b1) {
           return 1;
-        } else if (b1 > a1) {
+        }
+        else if (b1 > a1) {
           return -1;
-        } else {
+        }
+        else {
           return 0;
         }
       };
     }
 
+    let swapElem = null;
+    let lastValue = 0;
+
     for (let i = 0; i < this.length; i++) {
-      const swapElem = this[i];
-      let lastValue = i - 1;
+      swapElem = this[i];
+      lastValue = i - 1;
 
       while (lastValue >= 0 && cb(this[lastValue], swapElem) > 0) {
         this[lastValue + 1] = this[lastValue];
@@ -153,10 +137,8 @@ class MyArray {
       }
       this[lastValue + 1] = swapElem;
     }
-
     return this;
   }
-
   find(callback, thisArg) {
     let elemFind = null;
 
@@ -168,10 +150,8 @@ class MyArray {
       }
     }
   }
-
   slice(beginArg, endArg) {
     const resultArr = new MyArray();
-
     let begin = 0;
     let end = this.length;
 
@@ -199,30 +179,22 @@ class MyArray {
       resultArr[resultArr.length] = this[i];
       resultArr.length += 1;
     }
-
     return resultArr;
   }
-
   [Symbol.toPrimitive](hint) {
     switch (hint) {
     case 'string':
       return `Array element ${this[0]} is string`;
-
     case 'number':
       return this[0];
-
     default:
       return this[0];
     }
   }
-
   * [Symbol.iterator]() {
     for (let i = 0; i < this.length; i++) {
       yield this[i];
     }
   }
 }
-
 export default MyArray;
-
-
